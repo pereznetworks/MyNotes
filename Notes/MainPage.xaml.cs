db9e3a6fb4b958690f0401ef9def0d697fca76e3
pereznetworks <34382720+pereznetworks@users.noreply.github.com>
2025-11-12 17:43:54 -0800

    added static markup extentions and platform specific layout values

--- Notes/MainPage.xaml.cs
+++ Notes/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Notes;
+﻿// TODO: implement managing multiple notes
+
+namespace Notes;
 
 public partial class MainPage : ContentPage
 {
@@ -13,37 +15,18 @@ public partial class MainPage : ContentPage
                     editor.Text = File.ReadAllText(_fileName);
         }
     }
+
+    private void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, editor.Text);
+    }
+
+    private void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+        {
+            File.Delete(_fileName);
+        }
+        editor.Text = string.Empty;
+    }
 }
-
-// Original combined code, layout and event handling
-//
-// public MainPage()
-// {
-//     var notesHeading = new Label() { Text = "Notes", HorizontalOptions = LayoutOptions.Center, FontAttributes = FontAttributes.Bold };
-
-//     editor = new Editor() { Placeholder = "Enter your note", HeightRequest = 100 };
-//     editor.Text = File.Exists(_fileName) ? File.ReadAllText(_fileName) : string.Empty;
-
-//     var buttonsGrid = new Grid() { HeightRequest = 40.0 };
-//     buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
-//     buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(30.0, GridUnitType.Absolute) });
-//     buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
-
-//     var saveButton = new Button() { WidthRequest = 100, Text = "Save" };
-//     saveButton.Clicked += OnSaveButtonClicked;
-//     Grid.SetColumn(saveButton, 0);
-//     buttonsGrid.Children.Add(saveButton);
-
-//     var deleteButton = new Button() { WidthRequest = 100, Text = "Delete" };
-//     deleteButton.Clicked += OnDeleteButtonClicked;
-//     Grid.SetColumn(deleteButton, 2);
-//     buttonsGrid.Children.Add(deleteButton);
-
-//     var stackLayout = new VerticalStackLayout 
-//     { 
-//         Padding = new Thickness(30, 60, 30, 30),
-//         Children = { notesHeading, editor, buttonsGrid }
-//     };
-
-//     this.Content = stackLayout;
-// }
---
+++ Notes/SharedResources.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Notes
+{
+    static class SharedResources
+    {
+        public static readonly Color FontColor = Color.FromRgb(255, 255, 255);
+
+        public static readonly Color BackgroundColor = Color.FromRgb(27, 112, 182);
+    }
+
+}
+
+// this is not compatible on older windows, and older apple ios and macos 
+// build results in a CA1416 violation 
+// for best practic, need to using platform specific methods
