79f5959561950d153894f3472ebc8c66000e2208
pereznetworks <34382720+pereznetworks@users.noreply.github.com>
2026-04-03 16:07:50 -0700

    Added seperate view, NotePage, to edit, save and delete notes

--- AboutPage.xaml.cs
+++ AboutPage.xaml.cs
@@ -1,15 +1,15 @@
 namespace MyNotes;
 
-public partial class About : ContentPage
+public partial class AboutPage : ContentPage
 {
-	public About()
+	public AboutPage()
 	{
 		InitializeComponent();
 
 	}
 
-private async void LearnMore_Clicked(object sender, EventArgs e)
-{
+	private async void LearnMore_Clicked(object sender, EventArgs e)
+	{
         // Navigate to the specified URL in the system browser.
         await Launcher.Default.OpenAsync("https://aka.ms/maui");
     }
